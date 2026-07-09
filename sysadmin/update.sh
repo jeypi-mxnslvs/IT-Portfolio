b307afa375cd763103b5b973daac365feb7a1614
jeipyyy <janpaulmensalvas17@gmail.com>
2026-07-09 16:10:45 +0800

    Added another system update/upgrade for ./update.sh

--- sysadmin/update.sh
+++ sysadmin/update.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-echo "Starting to upgrade..."
-sudo pacman -Syu --noconfirm
+echo "=== Starting Arch Linux Update ==="
+echo "Started at: $(date)"
+echo ""
+
+# Update packages
+echo "Updating system packages..."
+sudo pacman -Syu --no-confirm
+
+echo "Cleaning up package cache..."
+sudo pacman -Sc --no-confirm
+
+echo ""
+echo "=== System update completed ==="
